180a28eb8e0040b583f8daf7fbeb2a3d0619af1a
Lucas Ezidro <you@example.com>
2021-07-12 18:11:39 -0300

    authentication with github

--- src/components/SignButton/index.tsx
+++ src/components/SignButton/index.tsx
@@ -1,18 +1,20 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 export function SignButton(){
-    const isUserLoggedIn = true;
+    const [session] = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
         type="button"
         className={styles.signButton}
+        onClick={() => signOut()}
 
         >
         <FaGithub color="#04d361"/>
-        Lucas Ezidro
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
     </button>
 
@@ -20,6 +22,7 @@ export function SignButton(){
         <button 
         type="button"
         className={styles.signButton}
+        onClick={() => signIn('github')}
 
         >
         <FaGithub color="#eba417"/>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 // esse é arquivo principal da aplicação
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'; // essa é uma forma de colocar o titulo de forma dinamica nas aplicações
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
@@ -40,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JBQ9gFKwPb8J8taIRS9nZR6')
 
   const product = {
@@ -54,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 *24
   }
 }
